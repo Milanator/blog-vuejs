bed8dfd18acbf172c33fbe17be5373a7e4904b93
Milan Navratil <navratil.milann@gmail.com>
2025-06-26 18:17:44 +0200

    [EDIT] Post - index

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -2,22 +2,22 @@
 import Posts from "~/components/post/posts.vue";
 </script>
 <template>
-  <form class="max-w-sm mx-auto">
-    <label
-      for="message"
-      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >Your post</label
-    >
-    <textarea
-      id="message"
-      rows="4"
-      class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="Leave a comment..."
-    ></textarea>
-  </form>
+  <div class="max-w-2xl mx-auto my-4">
+    <form class="mb-4">
+      <label
+        for="message"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Your post</label
+      >
+      <textarea
+        id="message"
+        rows="4"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Leave a comment..."
+      ></textarea>
+    </form>
 
-  <!-- Listing -->
-  <div class="max-w-3xl mx-auto my-4">
+    <!-- Listing -->
     <Posts />
   </div>
 </template>
