2113c3f0a8cced72e46c1274474289107aa9f8a0
Milan Navratil <navratil.milann@gmail.com>
2025-06-26 12:07:35 +0200

    [EDIT] Navbar

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import './assets/main.css'
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
